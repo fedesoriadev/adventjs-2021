b95cb37a3c298f36e2a489bc078a58ce76a7801a
Federico Soria <fedesoria.dev@gmail.com>
2022-05-27 00:29:15 -0300

    feat: day 17 - La locura de enviar paquetes

---
+++ 17/index.js
@@ -0,0 +1,34 @@
+const countPackages = (carriers, carrierID) => {
+  // Find carrier by name in carriers
+  const carrier = carriers.find(([name]) => name === carrierID)
+
+  // Destructuring selected carrier to constants by array position
+  const [, packages, subordinates] = carrier
+
+  // Sum carrier packages and recursive subordinates
+  return subordinates.reduce((sum, subordinate) => {
+    return sum + countPackages(carriers, subordinate)
+  }, packages)
+}
+
+const carriers = [
+  ['dapelu', 5, ['midu', 'jelowing']],
+  ['midu', 2, []],
+  ['jelowing', 2, []]
+]
+
+countPackages(carriers, 'dapelu') // 9
+// 5 de dapelu, 2 de midu y 2 de jelowing = 9
+
+const carriers2 = [
+  ['lolivier', 8, ['camila', 'jesuspoleo']],
+  ['camila', 5, ['sergiomartinez', 'conchaasensio']],
+  ['jesuspoleo', 4, []],
+  ['sergiomartinez', 4, []],
+  ['conchaasensio', 3, ['facundocapua', 'faviola']],
+  ['facundocapua', 2, []],
+  ['faviola', 1, []]
+]
+
+countPackages(carriers2, 'camila') // 15
+// 5 de camila, 4 de sergiomartinez, 3 de conchaasensio, 2 de facundocapua y 1 de faviola = 15
